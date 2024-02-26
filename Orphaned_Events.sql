52c3c973d3f0094305fd8fe8301a3b52732abfb4
Tyson <160985211+T-Jon@users.noreply.github.com>
2024-02-26 09:31:45 -0800

    Create Orphaned_Events.sql

---
+++ Orphaned_Events.sql
@@ -0,0 +1,11 @@
+-- Identifies quantity of events that are "orphaned" or are coming in w/o an associated _install event
+
+select
+	event_name,
+    exact_count_distinct(event_id) as orphaned_events
+from events_primary
+where install_id = '0'
+and date(date_received) > '2023-05-26'
+-- and date(date_received) between '2023-02-01' and '2023-02-19'
+group by event_name
+order by orphaned_events desc
