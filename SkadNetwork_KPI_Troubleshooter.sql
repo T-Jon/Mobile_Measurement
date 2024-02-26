8e1637037ad2778d88e2be0821bf54e04393d5f7
Tyson <160985211+T-Jon@users.noreply.github.com>
2024-02-26 08:39:52 -0800

    Update SkadNetwork_KPI_Troubleshooter.sql

--- SkadNetwork_KPI_Troubleshooter.sql
+++ SkadNetwork_KPI_Troubleshooter.sql
@@ -1,15 +1,17 @@
--- Do your users fire off your KPI events withing the required timeline thresholds to be measured within SkadNetwork attribution?\
--- This query will tell you how much time passes between the "register" for SKAD attribution aka _install event and your KPI event (this query uses 'FirstTimeDeposit' as that event)
+-- Do your users fire off your KPI events withing the required timeline thresholds to be measured within SkadNetwork attribution?
+-- This query will tell you how much time passes (avg) between the "register" for SKAD attribution aka _install event and your KPI event(s)
 
-select
-    e.event_value,
-    e.network_name,
-    exact_count_distinct(e.kochava_device_id) as deposit_count
-from events_primary e
-left join installs_primary i
-on i.install_id = e.install_id
-where e.event_name = 'FirstTimeDeposit'
-and date(e.date_received) > '2021-06-25'
-and date(i.date_occurred) = date(e.date_occurred)
-group by e.event_value, e.network_name
-order by deposit_count asc
+SELECT
+  e.event_name AS event_name,
+  AVG((TIMESTAMP_TO_SEC(e.date_occurred) - TIMESTAMP_TO_SEC(i.date_received))/3600) as avg_hour_diff
+FROM events_primary e
+JOIN installs_primary i
+ON e.kochava_device_id = i.kochava_device_id
+WHERE event_name IN
+  (
+    'QT_OAALeadSubmission_QM_LD',
+    'QT_UserIDCreation_QM_EV',
+    'QT_FirstDeposit_QM_EV'
+  )
+GROUP BY event_name
+ORDER BY event_name asc
